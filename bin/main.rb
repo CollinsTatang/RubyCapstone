8a191b9bf8e6018eb2a38e8ace1979ce19c9d30e
Collins Tatang <makungongcollins@gmail.com>
2021-03-06 16:53:30 +0100

    All project file created

---
+++ bin/main.rb
@@ -0,0 +1,29 @@
+# rubocop:disable Style/For
+# rubocop:disable Style/InfiniteLoop
+# rubocop:disable Lint/LiteralAsCondition
+
+require 'twitter'
+require 'open-uri'
+require_relative '../lib/api.rb'
+require_relative '../lib/meme_retriever.rb'
+
+while true
+  client = API.new
+  memes = MemeRetriever.new
+  time = 1800
+  meme_array = memes.get_memes
+  if meme_array.count.positive?
+    for x in meme_array
+      put client.post_meme(x)
+      puts "Meme posted in https://twitter.com/CollinsTatang1 Waiting #{time} seconds for next meme"
+      sleep(time) # Sleep 30 min between memes
+    end
+  else
+    puts 'Something went wrong with the API. Please try again later'
+    exit
+  end
+end
+
+# rubocop:enable Style/For
+# rubocop:enable Style/InfiniteLoop
+# rubocop:enable Lint/LiteralAsCondition
