12182790ec4d4fd90db4e3d6bd5c72a5dc3ef84f
Collins Tatang <makungongcollins@gmail.com>
2021-03-06 16:59:37 +0100

    rubocop error fix in api.rb file

--- lib/api.rb
+++ lib/api.rb
@@ -3,28 +3,28 @@ require 'open-uri'
 
 class API
   def initialize
-        @client = Twitter::Client.new do |config|
-            config.consumer_key = 'API-Key'
-            config.consumer_secret = 'API-Key secret'
-            config.access_token = 'Access Token'
-             config.access_token_secret = 'Access Token Secret'
-        end
+    @client = Twitter::Client.new do |config|
+      config.consumer_key = 'API-Key'
+      config.consumer_secret = 'API-Key secret'
+      config.access_token = 'Access Token'
+      config.access_token_secret = 'Access Token Secret'
     end
+  end
 
-    def post_meme(meme)
-        title = meme[0].to_s
-        link = meme[1].to_s
-        link = link.delete('["]')
-        if link.include?('https')
-            URI.open(link) do |image|
-                File.open('./meme.jpg', 'wb') do |file|
-                file.write(image.read)
-            end
+  def post_meme(meme)
+    title = meme[0].to_s
+    link = meme[1].to_s
+    link = link.delete('["]')
+    if link.include?('https')
+      URI.open(link) do |image|
+        File.open('./meme.jpg', 'wb') do |file|
+          file.write(image.read)
         end
-            @client.update_with_media(title.to_s, File.open('./meme.jpg', 'r+'))
-            true
-        else
-            'Argument provided is not a link'
-        end
+      end
+      @client.update_with_media(title.to_s, File.open('./meme.jpg', 'r+'))
+      true
+    else
+      'Argument provided is not a link'
     end
+  end
 end
